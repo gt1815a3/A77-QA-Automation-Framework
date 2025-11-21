bc289f44318bd2cae3996c0d4c612951320bd9eb
gt1815a3 <ricardo.lu@testpro.io>
2025-11-21 11:15:00 -0600

    URL Changed
    URL Changed

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 public class LoginTests extends BaseTest {
     @Test
     public void loginEmptyEmailPassword() {
-
+        //create fork
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -19,7 +19,8 @@ public class LoginTests extends BaseTest {
 
         // TODO (for students): Review the configuration as part of HW15
         
-        String url = "httpps://qa.koel.app/";
+        String url = "htpps://testpro.io/";
+
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
