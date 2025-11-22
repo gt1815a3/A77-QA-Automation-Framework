2b91eecfbeaf405f8581bfa9d76f0c372f27fde5
gt1815a3 <ricardo.lu@testpro.io>
2025-11-22 15:54:18 -0600

    Create Homework16.java
    First attempt.  I am getting an error that I do not undertand

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+    @Test
+    public void registrationNavigation(){
+        //create fork
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement registration = driver.findElement(By.xpath("//a[@href='registration']"));
+        registration.click();
+        WebElement RegisterText = driver.findElement(By.xpath("//span[@class='small'])"));
+        Assert.assertTrue(RegisterText.isDisplayed());
+        driver.quit();
+
+    }
+}
