63d81d04a0804c06b7a95ad526bcaf857f33b4e2
gt1815a3 <ricardo.lu@testpro.io>
2025-11-27 07:46:48 -0600

    Update LoginTests.java
    Just made a quick change and added a comment just to create another commit and pull request

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -11,6 +11,7 @@ public class LoginTests extends BaseTest {
     public void loginEmptyEmailPassword() {
         //create fork
 //      Added ChromeOptions argument below to fix websocket error
+        //test
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
