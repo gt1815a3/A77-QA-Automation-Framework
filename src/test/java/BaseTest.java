ed444fa77519677231fffb505b1be97cbae64030
gt1815a3 <ricardo.lu@testpro.io>
2025-12-01 08:33:09 -0600

    Finished Homework 18
    If you please look at my locators and let me know if there are better locators than the ones I am using.  I feel there will be more robust locators.
    
    Thanks

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,9 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 import java.util.UUID;
@@ -14,6 +16,8 @@ import java.util.UUID;
 public class BaseTest {
     public WebDriver driver;
     public String url = "https://qa.koel.app/";
+    public String randomName;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -58,5 +62,42 @@ public class BaseTest {
     public String generateRandomName() {
         //generate random ID, convert it to string, then remove the "-", no spaces
         return UUID.randomUUID().toString().replace("-", "").substring(0,5);
+    }
+
+
+
+    public String getAddToPlayListSuccessMsg() {
+        WebElement notifyMessage = driver.findElement(By.xpath("//div[@class='success show']"));
+        return notifyMessage.getText();
+    }
+
+    public void enterRandomNamePlaylist() {
+        WebElement playListName = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"));
+        playListName.clear();
+        randomName = generateRandomName();
+        playListName.sendKeys(randomName);
+        WebElement playListNameEnterBtn = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//button[@type='submit']"));
+        playListNameEnterBtn.click();
+    }
+
+    public void clickAddTo() {
+        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]"));
+        firstSong.click();
+    }
+
+    public void clickFirstSong() {
+        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
+        firstSong.click();
+    }
+
+    public void clickViewAllSongsBtn() {
+        WebElement viewAll = driver.findElement(By.xpath("//*[@id=\"searchExcerptsWrapper\"]/div/div/section[1]/h1/button"));
+        viewAll.click();
+    }
+
+    public void EnterSongInField(String song) {
+        WebElement searchSongField = driver.findElement(By.xpath("//input[@type='search']"));
+        searchSongField.clear();
+        searchSongField.sendKeys(song);
     }
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,10 +1,7 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework17 extends BaseTest{
-    public String randomName;
     @Test
     public void addSongToPlaylist() throws InterruptedException {
         navigatePage();
@@ -25,43 +22,5 @@ public class Homework17 extends BaseTest{
         getAddToPlayListSuccessMsg();
         Thread.sleep(2000);
         Assert.assertTrue(getAddToPlayListSuccessMsg().contains(randomName));
-
-
     }
-
-    public String getAddToPlayListSuccessMsg() {
-        WebElement notifyMessage = driver.findElement(By.xpath("//div[@class='success show']"));
-        return notifyMessage.getText();
-    }
-
-    public void enterRandomNamePlaylist() {
-        WebElement playListName = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"));
-        playListName.clear();
-        randomName = generateRandomName();
-        playListName.sendKeys(randomName);
-        WebElement playListNameEnterBtn = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//button[@type='submit']"));
-        playListNameEnterBtn.click();
-    }
-
-    public void clickAddTo() {
-        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]"));
-        firstSong.click();
-    }
-
-    public void clickFirstSong() {
-        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
-        firstSong.click();
-    }
-
-    public void clickViewAllSongsBtn() {
-        WebElement viewAll = driver.findElement(By.xpath("//*[@id=\"searchExcerptsWrapper\"]/div/div/section[1]/h1/button"));
-        viewAll.click();
-    }
-
-    public void EnterSongInField(String song) {
-        WebElement searchSongField = driver.findElement(By.xpath("//input[@type='search']"));
-        searchSongField.clear();
-        searchSongField.sendKeys(song);
-    }
-
 }
