fdf296eaec58a7450f5faaf7d9a7b42794c26e5c
gt1815a3 <ricardo.lu@testpro.io>
2025-12-02 15:53:17 -0600

    Added Implicit and Explicit waits
    Added Implicit and Explicit waits.  For some reason this is not compiling.  Can you tell me why?

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -29,6 +31,7 @@ public class BaseTest {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         url = BaseURL;
     }
     @AfterMethod
@@ -37,22 +40,28 @@ public class BaseTest {
     }
 
     public void clickOnLoginButton() {
-        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        loginButton.click();
+        //WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
     }
 
     public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
-        passwordField.click();
-        passwordField.clear();
-        passwordField.sendKeys(password);
+        //WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']"))).click();
+        //passwordField.click();
+        //passwordField.clear();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']"))).clear();
+        //passwordField.sendKeys(password);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']"))).sendKeys(password);
     }
 
     public void provideEmail(String email) {
-        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
-        emailField.click();
-        emailField.clear();
-        emailField.sendKeys(email);
+        //WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("///input[@type='email']"))).click();
+        //emailField.click();
+        //emailField.clear();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("///input[@type='email']"))).clear();
+        //emailField.sendKeys(email);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("///input[@type='email']"))).sendKeys(email);
     }
 
     public void navigatePage() {
@@ -67,36 +76,43 @@ public class BaseTest {
 
 
     public String getAddToPlayListSuccessMsg() {
-        WebElement notifyMessage = driver.findElement(By.xpath("//div[@class='success show']"));
+        WebElement notifyMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='success show']")));
         return notifyMessage.getText();
     }
 
     public void enterRandomNamePlaylist() {
-        WebElement playListName = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"));
-        playListName.clear();
+        //WebElement playListName = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"))).clear();
+        //playListName.clear();
         randomName = generateRandomName();
-        playListName.sendKeys(randomName);
-        WebElement playListNameEnterBtn = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//button[@type='submit']"));
-        playListNameEnterBtn.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='songResultsWrapper']//input[@type='text']"))).sendKeys(randomName);
+        //playListName.sendKeys(randomName);
+        //WebElement playListNameEnterBtn = driver.findElement(By.xpath("//*[@id='songResultsWrapper']//button[@type='submit']"));
+        //playListNameEnterBtn.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='songResultsWrapper']//button[@type='submit']"))).click();
     }
 
     public void clickAddTo() {
-        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]"));
-        firstSong.click();
+        //WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]"));
+        //firstSong.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"songResultsWrapper\"]/header/div[3]/span/button[2]"))).click();
     }
 
     public void clickFirstSong() {
-        WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
-        firstSong.click();
+        //WebElement firstSong = driver.findElement(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"));
+        //firstSong.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"songResultsWrapper\"]/div/div/div[1]/table/tr[1]/td[2]"))).click();
     }
 
     public void clickViewAllSongsBtn() {
-        WebElement viewAll = driver.findElement(By.xpath("//*[@id=\"searchExcerptsWrapper\"]/div/div/section[1]/h1/button"));
-        viewAll.click();
+        //WebElement viewAll = driver.findElement(By.xpath("//*[@id=\"searchExcerptsWrapper\"]/div/div/section[1]/h1/button"));
+        //viewAll.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"searchExcerptsWrapper\"]/div/div/section[1]/h1/button"))).click();
     }
 
     public void EnterSongInField(String song) {
-        WebElement searchSongField = driver.findElement(By.xpath("//input[@type='search']"));
+        //WebElement searchSongField = driver.findElement(By.xpath("//input[@type='search']"));
+        WebElement searchSongField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='search']")));
         searchSongField.clear();
         searchSongField.sendKeys(song);
     }
--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,7 +14,7 @@ public class Homework19 extends BaseTest{
         provideEmail("ricardo.lu@testpro.io");
         providePassword("DVNeY4ER");
         clickOnLoginButton();
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         checkPlayListExits();
 }
     public boolean isElementPresent(By locator) {
@@ -24,34 +25,44 @@ public class Homework19 extends BaseTest{
     //check to see if a playlist named Ricardo exits
         By elementLocator = By.xpath("//*[@id=\"playlists\"]/ul/li[4]/a[contains(text(), 'Ricardo')]");
         if (isElementPresent(elementLocator )){
-           //it exits so click on it
+           //it exists so click on it
             System.out.println("List already exits");
-            WebElement playList = driver.findElement(By.xpath("//*[@id=\"playlists\"]/ul/li[4]/a[contains(text(), 'Ricardo')]"));
-            playList.click();
-            WebElement deletePlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"));
-            deletePlayListBtn.click();
-            Thread.sleep(2000);
+            //WebElement playList = driver.findElement(By.xpath("//*[@id=\"playlists\"]/ul/li[4]/a[contains(text(), 'Ricardo')]"));
+            //playList.click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\\\"playlists\\\"]/ul/li[4]/a[contains(text(), 'Ricardo')]"))).click();
+            //WebElement deletePlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"));
+            //deletePlayListBtn.click();
+            //click on delete playlist button
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"))).click();
+            //Thread.sleep(2000);
             WebElement successNotification = driver.findElement(By.xpath("//*[@class=\"success show\"]"));
             String actualText = successNotification.getText();
             Assert.assertTrue(actualText.contains(playListName));
         } else {
             //play list does not exist so add it and click on it
             System.out.println("Playlist does not exist");
-            WebElement addPlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlists\"]/h1/i[@title='Create a new playlist']"));
-            addPlayListBtn.click();
-            Thread.sleep(2000);
-            WebElement newPlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlists\"]/nav/ul/li[@data-testid='playlist-context-menu-create-simple']"));
-            newPlayListBtn.click();
-            Thread.sleep(2000);
-            WebElement newPlayListNameEntryBox = driver.findElement(By.xpath("//*[@id=\"playlists\"]//input"));
-            newPlayListNameEntryBox.sendKeys(playListName);
-            Thread.sleep(1000);
-            newPlayListNameEntryBox.sendKeys(Keys.ENTER);
-            Thread.sleep(5000);
-            WebElement deletePlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"));
-            deletePlayListBtn.click();
-            Thread.sleep(2000);
-            WebElement successNotification = driver.findElement(By.xpath("//*[@class=\"success show\"]"));
+            //WebElement addPlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlists\"]/h1/i[@title='Create a new playlist']"));
+            //addPlayListBtn.click();
+            //click on Add Playlist button
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlists\"]/h1/i[@title='Create a new playlist']"))).click();
+            //Thread.sleep(2000);
+            //WebElement newPlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlists\"]/nav/ul/li[@data-testid='playlist-context-menu-create-simple']"));
+            //newPlayListBtn.click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlists\"]/nav/ul/li[@data-testid='playlist-context-menu-create-simple']"))).click();
+            //Thread.sleep(2000);
+            //WebElement newPlayListNameEntryBox = driver.findElement(By.xpath("//*[@id=\"playlists\"]//input"));
+            //newPlayListNameEntryBox.sendKeys(playListName);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlists\"]//input"))).sendKeys(playListName);
+            //Thread.sleep(1000);
+            //newPlayListNameEntryBox.sendKeys(Keys.ENTER);
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlists\"]//input"))).sendKeys(Keys.END);
+            //Thread.sleep(5000);
+            //WebElement deletePlayListBtn = driver.findElement(By.xpath("//*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"));
+            //deletePlayListBtn.click();
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"playlistWrapper\"]//button[@title='Delete this playlist']"))).click();
+            //Thread.sleep(2000);
+            //WebElement successNotification = driver.findElement(By.xpath("//*[@class=\"success show\"]"));
+            WebElement successNotification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class=\\\"success show\\\"]")));
             String actualText = successNotification.getText();
             Assert.assertTrue(actualText.contains(playListName));
         }//end else
