abbf426288fea7ab274c5bcc32776ce9d8a87451
gt1815a3 <ricardo.lu@testpro.io>
2025-12-30 16:30:59 -0600

    Fixed missing actions line
    Thank you for your previous hint.  I added the line you told me was missing.
    
    Can you please review it one more time?
    
    Again apologies for the multiple submissions.  I am quite new and trying to learn.
    
    Thanks

--- src/test/java/pagefactory/BasePage.java
+++ src/test/java/pagefactory/BasePage.java
@@ -16,6 +16,7 @@ public class BasePage {
     public BasePage(WebDriver givenDriver){
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
         PageFactory.initElements(driver, this);
     }
     protected void click(WebElement webElement){
